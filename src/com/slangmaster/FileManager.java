f77735a3166961ccae7925b12aee70f524bdd401
Nguyen Thanh An <thanhan030301@gmail.com>
2022-03-25 22:58:41 +0700

    add read file function

--- src/com/slangmaster/FileManager.java
+++ src/com/slangmaster/FileManager.java
@@ -1,4 +1,29 @@
 package com.slangmaster;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+
 public class FileManager {
+    public static ArrayList<Slang> readFile(String fileName) {
+        ArrayList<Slang> slangList = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line = reader.readLine();
+            while (line != null) {
+                try {
+                    slangList.add(Slang.parse(line));
+                } catch (ParseException ignored) {
+                }
+                finally {
+                    line = reader.readLine();
+                }
+            }
+        } catch (IOException ignored) {
+        }
+
+        return slangList;
+    }
 }
