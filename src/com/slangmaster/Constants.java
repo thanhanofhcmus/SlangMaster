4afe292930b48f76320236f0dc1f1d178aaef4f9
Nguyen Thanh An <thanhan030301@gmail.com>
2022-03-27 15:53:30 +0700

    change data file paths, add Constants

---
+++ src/com/slangmaster/Constants.java
@@ -0,0 +1,9 @@
+package com.slangmaster;
+
+public class Constants {
+    public static final String SAMPLE_SLANG_FILENAME = "./data/sample.txt";
+    public static final String SLANG_FILENAME = "./data/slang.txt";
+    public static final String DEFAULT_SLANG_FILENAME = "./data/slang_default.txt";
+    public static final String DEFINITION_INDEX_FILENAME = "./data/index_definition.csv";
+    public static final String MEANINGS_INDEX_FILENAME = "./data/index_meanings.csv";
+}
